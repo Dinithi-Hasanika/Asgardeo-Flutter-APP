97dfb16d27719a0c0916622a5c29640b7f1e1833
dinithih <dinithiliyanage.95@gmail.com>
2023-06-29 15:59:09 +0530

    drop down preferred MFA selection

--- lib/pages/setUpMFAPage.dart
+++ lib/pages/setUpMFAPage.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
+const List<String> itemList = <String>['DEFAULT','TOTP', 'Email', 'SMS'];
+
 class SetUpMFAPage extends StatefulWidget{
   final setPageIndex;
 
   SetUpMFAPage(this.setPageIndex);
   @override
   State<SetUpMFAPage> createState() {
-    // TODO: implement createState
     return _SetUpMFAPage(this.setPageIndex);
   }
 
@@ -14,13 +15,47 @@ class SetUpMFAPage extends StatefulWidget{
 
 class _SetUpMFAPage extends State<SetUpMFAPage>{
   final setPageIndex;
+  String dropDownValue = itemList.first;
 
   _SetUpMFAPage(this.setPageIndex);
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Center(child: Column(
-        children: [Text('Set Up MFA Page'),
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Text('Set Up MFA', style: TextStyle(fontSize: 25)),
+          SizedBox(height: 40),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Preferred MFA Option', style: TextStyle(fontSize: 18)),
+              SizedBox(width: 25,),
+              DropdownButton(
+                value: this.dropDownValue,
+                underline: Container(
+                  height: 2,
+                  color: Colors.orange,
+                ),
+                items: itemList.map<DropdownMenuItem<String>>((String value){
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+                onChanged: (String? value) {
+              print(value);
+              setState(() {
+                this.dropDownValue = value!;
+              });
+                }),
+          ],),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              setPageIndex(2);
+            },
+            child: Text('Save'),
+          ),
           SizedBox(height: 20),
           ElevatedButton(
             onPressed: () {
