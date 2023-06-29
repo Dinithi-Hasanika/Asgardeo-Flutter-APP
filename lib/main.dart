035f704efaac9775a43962ab7a267a4bdec3fd3a
dinithih <dinithiliyanage.95@gmail.com>
2023-06-29 13:03:18 +0530

    handle expired refresh token

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:asgardeo_flutter_app/pages/setUpMFAPage.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_appauth/flutter_appauth.dart';
