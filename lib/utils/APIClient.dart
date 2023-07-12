cd2dbb6e6411aa74418275ceaafcf20ab55d4f99
dinithih <dinithiliyanage.95@gmail.com>
2023-07-12 16:22:05 +0530

    add constants

--- lib/utils/APIClient.dart
+++ lib/utils/APIClient.dart
@@ -1,12 +1,13 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'APIClientConstants.dart' as Constants;
 
 class APIClient {
 
   Future<http.Response> httpGet(String url, String? accessToken) {
     final result = http.get(
       Uri.parse(url),
-      headers: {'Authorization': 'Bearer $accessToken'},
+      headers: {Constants.authorization: '${Constants.bearer} $accessToken'},
     );
     return result;
   }
@@ -14,7 +15,8 @@ class APIClient {
   Future<http.Response> httPatch(String url, String? accessToken, data){
     final result = http.patch(
       Uri.parse(url),
-        headers: {'Authorization': 'Bearer $accessToken', 'Content-Type': 'application/scim+json'},
+        headers: {Constants.authorization: '${Constants.bearer} $accessToken',
+          Constants.contentType: Constants.contentTypeJson},
         body: json.encode(data)
     );
     return result;
---
+++ lib/utils/APIClientConstants.dart
@@ -0,0 +1,4 @@
+const authorization = 'Authorization';
+const bearer = 'Bearer';
+const contentType = 'Content-Type';
+const contentTypeJson = 'application/scim+json';
