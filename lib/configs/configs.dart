948b278918027252c7d32eb3b9097423f394eb0d
dinithih <dinithiliyanage.95@gmail.com>
2023-07-14 08:45:37 +0530

    config to add default photo

--- lib/configs/configs.dart
+++ lib/configs/configs.dart
@@ -3,3 +3,6 @@ const redirectUrl = 'wso2.asgardeo.flutterapp://login-callback';
 const organizationName = '<organization-name>';
 const spName = '<sp-name>';
 const externalAPIEndpoint = '<external-api-url>';
+
+//optional
+const defaultPhotoURL = 'https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI=';
