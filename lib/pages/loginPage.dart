f9da464f6a65c762d2df96896410ef731276e6c2
dinithih <dinithiliyanage.95@gmail.com>
2023-07-25 12:50:17 +0530

    optional create account button

--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -31,19 +31,22 @@ class LogInPage extends StatelessWidget {
           ),
             const SizedBox(height: 20),
             Container(
-              height: 44.0,
-              width: 180.0,
-              decoration: BoxDecoration(
-                  color: Color(constants.primaryColor),
-                  borderRadius: BorderRadius.circular(constants.buttonRadius)),
+              height: 42.0,
+              width: 178.0,
+              color: Theme.of(context).scaffoldBackgroundColor,
               child: ElevatedButton(
                 onPressed: () async {
                   await signUpFunction();
                 },
                 style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.transparent,
-                    shadowColor: Colors.transparent),
-                child: const Text(constants.signUp, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                    side: const BorderSide(
+                      width: 1.0,
+                      color: Color(constants.primaryColor),
+                    ),
+                    shape:RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(constants.buttonRadius),
+                    )),
+                child: const Text(constants.signUp, style: TextStyle(fontWeight: FontWeight.bold)),
               ),
             ),
           ]
